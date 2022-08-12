b8850ac73fc7cf4f1194fdfb3ba842ac06fa4bdd
Muspi Merol <74518716+CNSeniorious000@users.noreply.github.com>
2022-08-12 22:44:37 +0800

    normalize Chinese

---
+++ data/normalize.py
@@ -0,0 +1,28 @@
+from glob import glob
+
+
+def normalize(string: str):
+    return (
+        string
+        .replace(",", "，")
+        # .replace(":", "：")  # https://
+        # .replace("(", "（")  # ![]()
+        # .replace(")", "）")  # ![]()
+        .replace("?", "？")
+        .replace(";", "；")
+        .replace("？)", "?)")  # (yyyy-?)
+        .replace("？）", "?）")  # （yyyy-?）
+        .replace("—", "-")
+        .replace("~", "-")
+        .replace("\r\n", "\n")
+    )
+
+
+if __name__ == '__main__':
+    documents = glob("**/*.md", recursive=True)
+    for document in documents:
+        with open(document, "r+", encoding="utf-8") as md:
+            text = md.read()
+            md.seek(0)
+            md.write(normalize(text))
+            md.truncate()

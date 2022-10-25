8e56d2ed91bd9c3289ed490849c74402f4b78fff
Muspi Merol <74518716+CNSeniorious000@users.noreply.github.com>
2022-10-25 12:44:40 +0800

    bug fix: add service worker JavaScript file

---
+++ static/sw.js
@@ -0,0 +1,14 @@
+self.addEventListener("install", (ev) => {
+    ev.waitUntil(
+        caches.open("bnu120").then((cache) => {
+            cache.addAll([
+                "https://unpkg.com/@popperjs/core@2/dist/umd/popper.js",
+                "https://fonts.googleapis.com/css2?family=Noto+Serif+SC:wght@200;300;400;500;600;700;900&display=swap",
+            ]).then(console.log)
+        })
+    )
+})
+
+self.addEventListener("fetch", (ev) => {
+    ev.respondWith(caches.match(ev.request).then((response) => response ?? fetch(ev.request)))
+})

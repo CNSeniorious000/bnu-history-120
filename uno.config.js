c337f4ab6b761c53dc936fb5750a97fddef3786d
CNSeniorious000 <wjj123345567@icloud.com>
2023-10-08 18:40:19 +0800

    feat: setup unocss and add `More` button

---
+++ uno.config.js
@@ -0,0 +1,12 @@
+import presetIcons from "@unocss/preset-icons"
+import presetUno from "@unocss/preset-uno"
+
+export default {
+    cli: {
+        entry: {
+            patterns: ["./templates/*.jinja2"],
+            outFile: "./static/uno.css"
+        }
+    },
+    presets: [presetUno({ dark: "media" }), presetIcons()]
+}
